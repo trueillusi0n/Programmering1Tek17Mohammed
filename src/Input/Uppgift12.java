1c3b74c13db5c86b876aedc2194b5a260e8394ee
trueillusi0n <mohammad.saberi@elev.ga.ntig.se>
2018-10-04 11:00:54 +0200

    Tek17

--- src/Input/Uppgift12.java
+++ src/Input/Uppgift12.java
@@ -5,30 +5,67 @@ import java.util.Scanner;
 public class Uppgift12 {
 
 	public static void main(String[] args) {
-		
-		int dec_num, rem, quot, i=1, j;
-		int oct_num[] = new int[100];
-		Scanner scan = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("Input a Decimal Number: ");
-		dec_num = scan.nextInt();
 		
-		quot = dec_num;
+		int integerNumber = scanner.nextInt();
+/*		0 = 0
+		1 = 1
+		2 = 10
+		3 = 11
+		4 = 100
+		5 = 101
+		6 = 110
+		7 = 111
+		8 = 1000
+		9 = 1001
 		
-		while(quot != 0)
-		{
-			oct_num[i++] = quot%8;
-			quot = quot/8;
-		}
-		System.out.print("Octal number is: ");
-		for(j=i-1; j>0; j--)
-		{
-			
-			System.out.print(oct_num[j]);
-			
-		}
-		System.out.print("\n");
-
+		128 64 32 16 8 4 2 1
+		
+		
+		
+		0 = 0 
+		1 = 1 
+		2 = 2
+		3 = 3 
+		4 = 4
+		5 = 5 
+		6 = 6
+		7 = 7
+		8 = 10
+		9 = 11
+		10 = 12
+		11 = 13
+		12 = 14
+		13 = 15
+		16 = 20
+		
+		
+		64*8 64 8 1
+		
+		
+		8^4 8^3 8^2 8^1 8^0
+		
+		
+	*/	
+		/*
+		123 = 123 - 64 = 59
+		
+		8^2
+		1
+		
+		8^1
+		59 - 56 = 3
+		7
+		
+		8^0 
+		3
+		
+		173
+		*/
+		
+		
 	}
 
 }
